0257b5dc62d736bed7e5bb4ae7bb60bade2a4c2e
unyon <michalloran@hotmail.com>
2016-01-15 22:35:43 +0000

    rake db

--- db/migrate/20160115223302_create_names.rb
+++ db/migrate/20160115223302_create_names.rb
@@ -1,12 +1,10 @@
 class CreateNames < ActiveRecord::Migration
   def change
-  create_table :names do |t|
+    create_table :names do |t|
     	t.string :person_name
     	t.string :email
-
-        t.timestamps
-   end
-
+    	t.timestamps
+    end
   end
 end
-
+    
